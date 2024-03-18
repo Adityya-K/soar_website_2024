6c8466459681631c10c048d6f641619e62f712a7
Adityya Kaushal <adityyakaushal@gmail.com>
2024-03-18 18:12:45 -0400

    added camera and fixed navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <main>
-      <div className='bg-[#161032] flex justify-center h-[5vh] items-center text-white'>
+      <div className='fixed z-50 top-0 bg-[#161032] flex justify-center h-[5vh] w-screen items-center text-white'>
         <h1 className='pr-10 pl-10'>Applications</h1>
         <h1 className='pr-10 pl-10'>Gallery</h1>
         <h1 className='pr-10 pl-10'>Your Chairs</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 import Navbar from './components/Navbar'
+import camera from '../public/Camera.png'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className='bg-[#161032] h-[95vh]'>
-        <svg className="absolute bottom-0" id="trapezoid" viewbox="0 0 100 15" preserveAspectRatio="none" height="200px" width="100%">
+      <div className='bg-[#161032] h-[95vh] min-h-[600px]'>
+        <svg className="absolute bottom-0 z-1" id="trapezoid" viewBox="0 0 100 15" preserveAspectRatio="none" height="200px" width="100%">
           <path d="M20,0
                   L80,0
                   L100,10
                   L100,15
                   L0,15
-                  L0,10z" fill="red"></path>
+                  L0,10z" fill="#931621"></path>
         </svg>
+        <Image src={camera} width={200} alt="camera" className="absolute bottom-[185px] left-1/2 transform -translate-x-1/2 "/>
       </div>
-      <div className='h-screen'>
+      <div className='h-screen'> 
       </div>
     </main>
   )
