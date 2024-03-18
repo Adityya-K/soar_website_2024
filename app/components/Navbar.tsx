0fcaa6ce6f2f6c5dd525fb4d2c027905b0411da2
Adityya Kaushal <adityyakaushal@gmail.com>
2024-03-18 09:25:42 -0400

    started working on home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <main>
+      <div className='bg-[#161032] flex justify-center h-[5vh] items-center text-white'>
+        <h1 className='pr-10 pl-10'>Applications</h1>
+        <h1 className='pr-10 pl-10'>Gallery</h1>
+        <h1 className='pr-10 pl-10'>Your Chairs</h1>
+      </div>
+    </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import Image from 'next/image'
-import poster from '../public/poster.jpg'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <main>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h1 className="text-white font-bold underline text-8xl">SOAR</h1>
-        <h1 className="text-white text-xl pt-5">2024 conference</h1>
+      <Navbar />
+      <div className='bg-[#161032] h-[95vh]'>
+        <svg className="absolute bottom-0" id="trapezoid" viewbox="0 0 100 15" preserveAspectRatio="none" height="200px" width="100%">
+          <path d="M20,0
+                  L80,0
+                  L100,10
+                  L100,15
+                  L0,15
+                  L0,10z" fill="red"></path>
+        </svg>
       </div>
-      <Image  
-        src={poster}
-        alt="background"
-        layout="fill"
-        objectFit='cover'
-        className="z-[-1]"
-      />
+      <div className='h-screen'>
+      </div>
     </main>
   )
 }
