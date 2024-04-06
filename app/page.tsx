05e78e56cd8c50a8503c2eba1d537e5e81515ae3
Adityya Kaushal <adityyakaushal@gmail.com>
2024-04-06 11:52:43 -0400

    added elements to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import Navbar from './components/Navbar'
-import camera from '../public/Camera.png'
+import soar from '../public/soar-logo.png'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
+      <div className='blob'></div>
+      <div className='blob-1'></div>
       <Navbar />
-      <div className='bg-[#161032] flex h-screen min-h-[600px] w-screen justify-center items-center'>
-        <div>
-          <h1 className='text-white text-center font-bold text-9xl'>SOAR 2024</h1>
+      <div className='bg-[#161032] flex flex-wrap h-screen min-h-[600px] w-screen justify-center items-center'>
+        <div className='relative z-25'>
+          <h1 className='text-white text-center pl-10 font-bold text-9xl'>SOAR 2024</h1>
           <h1 className='text-white text-center font-bold text-xl'>Welcome to SOAR 2024</h1>
         </div>
-        <div className='pl-[150px] pr-[150px]'>
-          <Image src={camera} height={500} alt='image' />
+        <div className='relative z-25 pl-[150px] pr-[150px]'>
+          <Image src={soar} height={500} alt='image' />
         </div>
       </div>
-      <div className='h-screen'>
-      </div>
+      <div className='h-screen w-screen bg-white'></div>
     </main>
   )
 }
