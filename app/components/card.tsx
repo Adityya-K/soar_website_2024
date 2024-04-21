7868d3390ddcc403f7630d4634341f82e245592d
Adityya Kaushal <adityyakaushal@gmail.com>
2024-04-21 08:44:13 -0400

    finished website for now

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -6,23 +6,23 @@ export default function card(props: any) {
     <main>
       {
         props.left ?
-          <div className='bg-[#161032] rounded border-[1px] border-solid border-white text-white grid grid-cols-5 gap-4 h-[100%]'>
-            <div className="col-span-2">
-              <Image src={props.img} alt="image" style={{ padding: "10px", overflow: "hidden" }} />
+          <div className='bg-[#161032] rounded-2xl border-[1px] border-solid border-white text-white grid grid-cols-5 gap-4 h-[100%]'>
+            <div className="col-span-2 flex items-center justify-center">
+              <Image src={props.img} alt="image" style={{ padding: "10px", overflow: "hidden", objectFit: "cover", height: "100%", borderRadius: "1rem" }} />
             </div>
             <div className='col-span-3 pl-10'>
-              <h1 className='text-4xl pt-10'>{props.title}</h1>
-              <p className="pt-5 text-xl">{props.body}</p>
+              <h1 className='text-4xl pt-10 pr-10'>{props.title}</h1>
+              <div className="pt-5 text-l pb-10 pr-10">{props.children}</div>
             </div>
           </div>
           :
-          <div className='bg-[#161032] rounded border-[1px] border-solid border-white text-white grid grid-cols-5 gap-4 h-[100%]'>
+          <div className='bg-[#161032] rounded-2xl border-[1px] border-solid border-white text-white grid grid-cols-5 gap-4 h-[100%]'>
             <div className='col-span-3 pr-10'>
-              <h1 className='text-4xl text-right pt-10'>{props.title}</h1>
-              <p className="pt-5 text-right text-xl">{props.body}</p>
+              <h1 className='text-4xl text-right pt-10 pl-10'>{props.title}</h1>
+              <div className="pt-5 pb-10 pl-5 text-right text-l">{props.children}</div>
             </div>
-            <div className="col-span-2" >
-              <Image src={props.img} alt="image" layout="responsive" style={{ padding: "10px", overflow: "hidden" }} />
+            <div className="col-span-2 flex items-center justify-center" >
+              <Image src={props.img} alt="image" style={{ padding: "10px", overflow: "hidden", objectFit: "cover", height: "100%", borderRadius: "1rem" }} />
             </div>
           </div>
       }
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,17 @@ import Footer from './components/Footer'
 import soar from '../public/soar-logo.png'
 import Image from 'next/image'
 import Card from './components/card'
+import img1 from '../public/soar-1.jpg'
+import img2 from '../public/soar-2.png'
+import img3 from '../public/soar-3.png'
 
 export default function Home() {
   return (
     <main>
+      {/*
+      <div className='fixed bg-red-700 h-[50px] w-screen z-50 flex items-center justify-center'>
+        <p className='text-xl text-white'>SIGN UP HERE: link</p>
+      </div> */}
       <div className='blob'></div>
       <div className='blob-1'></div>
       <div className='bg-[#161032] flex flex-wrap h-screen min-h-[600px] w-screen justify-center items-center'>
@@ -19,13 +26,19 @@ export default function Home() {
       </div>
       <div className='w-screen p-10 grid xl:grid-cols-2 grid-cols-1 grid-rows-3 gap-10 background'>
         <div className="xl:w-[100%] w-[80%] relative left-[50%] -translate-x-[50%]">
-          <Card title={"Hello"} body={"bye bye"} img={soar} left={false} />
+          <Card title={"What is SOAR?"} img={img1} left={false}>
+            <p>Suddenly Opportunities Are Reachable, <b>SOAR</b> is a leadership conference for grade 7 & 8 students in the Peel District School Board. Our mission statement is <q className='underline hover:decoration-dotted decoration-sky-500 decoration-2'>bridging the gap from middle school to high school for grade 7 and 8 students, creating the leaders of tomorrow.</q> This conference equips students with necessary skills such as communication, leadership, and a burning passion, preparing students for a rewarding high school experience. We strive to provide a valuable experience for each delegate and equip them with exceptional skill sets, positively impacting our communities and initiating leadership.</p>
+          </Card>
         </div>
         <div className="xl:row-start-2 xl:col-start-2 xl:w-[100%] w-[80%] relative left-[50%] -translate-x-[50%]">
-          <Card title={"Hello"} body={"bye bye"} img={soar} left={true} />
+          <Card title={"Why should students attend SOAR?"} img={img2} left={true}>
+            <p>If you want your child to be part of a transformative summer program designed for academic excellence and development, SOAR is the conference to attend. With a focus on personalized attention and collaborative activities, SOAR ensures your child receives individualized support to build confidence and crucial life skills. This unique experience fosters lasting friendships and prepares students for future success, making SOAR the best choice for parents in pursuit of a program that uniquely shapes their child&apos;s growth and potential.</p>
+          </Card>
         </div>
         <div className="xl:row-start-3 xl:w-[100%] w-[80%] relative left-[50%] -translate-x-[50%]">
-          <Card title={"Hello"} body={"bye bye"} img={soar} left={false} />
+          <Card title={"WHEN + WHERE?"} img={img3} left={false}>
+            <p>The SOAR 2024 conference will take place at <u className='underline hover:decoration-dotted decoration-sky-500 decoration-2'> Turner Fenton Secondary School on Saturday, June, 1st, 2024. </u> Delegates will be expected to arrive at <b> 7:00 am. </b></p>
+          </Card>
         </div>
       </div>
       <Footer />
